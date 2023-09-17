2ee37d67eed4288e569e61c1e12cbf5ca5f8de2f
maaz khan <maazkhaansofteng@gmail.com>
2023-09-17 22:50:20 +0500

    add stroy componenet

---
+++ src/app/story/story.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-story',
+  templateUrl: './story.component.html',
+  styleUrls: ['../app.component.css']
+})
+export class StoryComponent {
+  users = [
+    { username: 'usernick1' },
+    { username: 'usernick2' },
+    { username: 'usernick3' },
+    { username: 'usernick4' },
+    { username: 'usernick5' },
+    { username: 'usernick6' },
+    { username: 'usernick7' },
+    { username: 'usernick8' },
+    { username: 'usernick9' },
+    { username: 'usernick10' },
+    { username: 'usernick11' },
+    { username: 'usernick12' },
+    { username: 'usernick13' },
+    { username: 'usernick14' },
+    { username: 'usernick15' }
+  ];
+}
