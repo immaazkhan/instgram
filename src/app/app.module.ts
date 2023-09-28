3f0d59f7d677bfebf87e00179079ddb68b83e0ec
maaz khan <maazkhaansofteng@gmail.com>
2023-09-28 15:11:02 +0500

    fixing story component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoryComponent } from './story/story.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StoryComponent
+    StoryComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
